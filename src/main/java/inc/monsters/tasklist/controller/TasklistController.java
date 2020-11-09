afa216d9c4e4ab24b0e994c3f6ce35b516990edd
tcurtis <tcurtis@7ae9ff92-2d03-4203-9fff-52602ee40195>
2020-11-09 01:57:34 +0000

    Figured out how to pass GET parameters. Misc cleanup.
    git-svn-id: svn://192.168.1.123/srv/svn/Work/tasklist/trunk@21 7ae9ff92-2d03-4203-9fff-52602ee40195

--- src/main/java/inc/monsters/tasklist/controller/TasklistController.java
+++ src/main/java/inc/monsters/tasklist/controller/TasklistController.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inc.monsters.tasklist.controller;
 
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import inc.monsters.tasklist.model.entity.Tasklist;
-import inc.monsters.tasklist.model.service.TasklistService;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -20,31 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class TasklistController {
-    
-private List<Tasklist> tasklists;
-    private TasklistService tasklistService;
-    private Tasklist tasklist;
 
-    public TasklistController(TasklistService tasklistService, Tasklist tasklist) {
-        this.tasklistService = tasklistService;
-        this.tasklist = tasklist;
-    }
-    
-    @GetMapping("/tasklistMgmt")
-    public String TasklistListing(Model model) {
-        List<Tasklist> list = tasklistService.findAll();
-        model.addAttribute("tasklists", list);
-        model.addAttribute("tasklist", tasklist);
+    @GetMapping("/tasklist")
+    public String showTasklist(@RequestParam(value= "id", required = false) Integer id, Model model) {
+        System.out.println(id);
         
-        return "tasklists";
-    }
-    
-    @PostMapping("/tasklistMgmt")
-    public String addTasklist(@ModelAttribute Tasklist tasklist, Model model) {
-        tasklistService.save(tasklist);
-        List<Tasklist> list = tasklistService.findAll();
-        model.addAttribute("tasklists", list);
-        
-        return "tasklists";
+        return "tasklist";
     }
 }
---
+++ src/main/java/inc/monsters/tasklist/controller/TasklistManagementController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inc.monsters.tasklist.controller;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import inc.monsters.tasklist.model.entity.Tasklist;
+import inc.monsters.tasklist.model.service.TasklistService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author tcurtis
+ */
+@Controller
+public class TasklistManagementController {
+    
+private List<Tasklist> tasklists;
+    private TasklistService tasklistService;
+    private Tasklist tasklist;
+
+    public TasklistManagementController(TasklistService tasklistService, Tasklist tasklist) {
+        this.tasklistService = tasklistService;
+        this.tasklist = tasklist;
+    }
+    
+    @GetMapping("/tasklistMgmt")
+    public String TasklistListing(Model model) {
+        List<Tasklist> list = tasklistService.findAll();
+        model.addAttribute("tasklists", list);
+        model.addAttribute("tasklist", tasklist);
+        
+        return "tasklistManagement";
+    }
+    
+    @PostMapping("/tasklistMgmt")
+    public String addTasklist(@ModelAttribute Tasklist tasklist, Model model) {
+        tasklistService.save(tasklist);
+        List<Tasklist> list = tasklistService.findAll();
+        model.addAttribute("tasklists", list);
+        
+        return "tasklistManagement";
+    }
+}
