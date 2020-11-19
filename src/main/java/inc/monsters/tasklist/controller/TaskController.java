a67ec9193275cd289d66d5de4d5d10acd683214a
tcurtis <tcurtis@7ae9ff92-2d03-4203-9fff-52602ee40195>
2020-11-19 14:48:03 +0000

    Added post to TaskController
    git-svn-id: svn://192.168.1.123/srv/svn/Work/tasklist/trunk@28 7ae9ff92-2d03-4203-9fff-52602ee40195

--- src/main/java/inc/monsters/tasklist/controller/TaskController.java
+++ src/main/java/inc/monsters/tasklist/controller/TaskController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -26,5 +27,12 @@ public class TaskController {
         task.ifPresent(taskToEdit -> model.addAttribute("task", taskToEdit));
         
         return "editTask";
+    }
+    
+    @PostMapping("/task/save")
+    public String saveTask() {
+        System.out.println("Testing");
+        
+        return "redirect:/";
     }
 }
