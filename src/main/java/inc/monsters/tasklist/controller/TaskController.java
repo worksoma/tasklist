b156c3260e264139cb10068cbd52938cfacb17cd
tcurtis <tcurtis@7ae9ff92-2d03-4203-9fff-52602ee40195>
2020-11-19 19:32:44 +0000

    Can now edit tasks.
    git-svn-id: svn://192.168.1.123/srv/svn/Work/tasklist/trunk@29 7ae9ff92-2d03-4203-9fff-52602ee40195

--- src/main/java/inc/monsters/tasklist/controller/TaskController.java
+++ src/main/java/inc/monsters/tasklist/controller/TaskController.java
@@ -2,10 +2,14 @@ package inc.monsters.tasklist.controller;
 
 import inc.monsters.tasklist.model.service.TaskService;
 import inc.monsters.tasklist.model.entity.Task;
+import inc.monsters.tasklist.form.TaskForm;
+import inc.monsters.tasklist.model.service.TasklistService;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,23 +20,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TaskController {
     private final TaskService taskService;
+    private final TasklistService tasklistService;
+    private TaskForm taskForm;
 
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, TasklistService tasklistService, TaskForm taskForm) {
         this.taskService = taskService;
+        this.tasklistService = tasklistService;
+        this.taskForm = taskForm;
     }
     
     @GetMapping("/task/edit")
     public String editTask(@RequestParam(value= "id", required = false) Long id, Model model) {
         Optional<Task> task =  taskService.findById(id);
-        task.ifPresent(taskToEdit -> model.addAttribute("task", taskToEdit));
+        task.ifPresent(taskToEdit -> model.addAttribute("task", toForm(taskToEdit)));
         
         return "editTask";
     }
     
     @PostMapping("/task/save")
-    public String saveTask() {
-        System.out.println("Testing");
+    public String saveTask(@ModelAttribute TaskForm taskForm, Model model) {
+        System.out.println("Testing: " + taskForm.getTasklistId() + taskForm.getTitle());
+        var tasklistId = taskForm.getTasklistId();
         
-        return "redirect:/";
+        taskService.save(toEntity(taskForm));
+        
+        TaskForm newForm = new TaskForm();
+        newForm.setTasklistId(tasklistId);
+        List<Task> tasks = taskService.findByTasklistId(tasklistId);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("taskForm", newForm);
+        
+        return "redirect:/tasklist?id=" + taskForm.getTasklistId();
+    }
+    
+    private TaskForm toForm(Task task) {
+        return new TaskForm(task.getId(), task.getTitle(), task.getTasklist().getId());
+    }
+    
+    private Task toEntity(TaskForm taskForm) {
+        Task task = new Task();
+        
+        task.setId(taskForm.getId());
+        task.setTitle(taskForm.getTitle());
+        task.setTasklist(tasklistService.getOne(taskForm.getTasklistId()));
+        
+        return task;
     }
 }
--- src/main/java/inc/monsters/tasklist/form/TaskForm.java
+++ src/main/java/inc/monsters/tasklist/form/TaskForm.java
@@ -1,6 +1,8 @@
 package inc.monsters.tasklist.form;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
  * @author tcurtis
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Component
 public class TaskForm {
     private Long id;
