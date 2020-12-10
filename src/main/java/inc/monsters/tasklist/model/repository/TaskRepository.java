bafd7728beb2c462e70a33d8cf44976e339f0996
tcurtis <tcurtis@7ae9ff92-2d03-4203-9fff-52602ee40195>
2020-12-10 16:03:27 +0000

    Cleaned up comments.
    git-svn-id: svn://192.168.1.123/srv/svn/Work/tasklist/trunk@44 7ae9ff92-2d03-4203-9fff-52602ee40195

--- src/main/java/inc/monsters/tasklist/model/repository/TaskRepository.java
+++ src/main/java/inc/monsters/tasklist/model/repository/TaskRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inc.monsters.tasklist.model.repository;
 
 import org.springframework.stereotype.Repository;
@@ -16,5 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    
     public List<Task> findByTasklistId(Long id);
 }
